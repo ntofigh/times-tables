dc41a40d4075990099eb857c3cf06c118dba1f6e
nora <no.tofigh@gmail.com>
2016-07-02 23:46:09 -0400

    initial commit
    also final commit

--- times tables/ViewController.swift
+++ times tables/ViewController.swift
@@ -8,14 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var sliderValue: UISlider!
+   
+    @IBAction func sliderMoved(sender: AnyObject) {
+        
+        table.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+            return 20
+        }
+        
+        
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+            
+            let y = Int(sliderValue.value)
+            
+            cell.textLabel?.text = " \(y) x \(indexPath.row + 1) = " + String(Int(sliderValue.value) * Int(indexPath.row + 1))
+            
+            return cell
+            
+        }
+        
+
+    
+    
+
+     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
